85830e828007909a9cab8ae6fc2ba22d60652b2a
Alexander Leschanz <80907917+Creative-Difficulty@users.noreply.github.com>
2025-04-30 13:40:48 +0200

    fix?: flake.nix indentation

--- flake.nix
+++ flake.nix
@@ -15,34 +15,35 @@
     };
   };
 
-  outputs = { self, nixpkgs, disko, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, disko, home-manager, ... }@inputs: (
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {    
-    nixosConfigurations = {
-      nixosbtw = nixpkgs.lib.nixosSystem {
-        inherit system;
-        # Give the configuration access to inputs of the flake and outputs
-        specialArgs = { inherit inputs; };
-        modules = [
-          disko.nixosModules.disko
+      nixosConfigurations = {
+        nixosbtw = nixpkgs.lib.nixosSystem {
+          inherit system;
+          # Give the configuration access to inputs of the flake and outputs
+          specialArgs = { inherit inputs; };
+          modules = [
+            disko.nixosModules.disko
 
-          ./hosts/nixosbtw/hardware-configuration.nix
-          ./hosts/nixosbtw/configuration.nix
-        ];
-      }; # nixosbtw
-    }; # nixosconfigurations
+            ./hosts/nixosbtw/hardware-configuration.nix
+            ./hosts/nixosbtw/configuration.nix
+          ];
+        }; # nixosbtw
+      }; # nixosconfigurations
 
-    homeConfigurations."alex" = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
-      # Specify your home configuration modules here, for example,
-      # the path to your home.nix.
-      modules = [ ./users/alex.nix ];
-      # Optionally use extraSpecialArgs
-      # to pass through arguments to home.nix
-      # extraSpecialArgs = { inherit inputs; };
-    };
-  }; # let ... in ...
+      homeConfigurations."alex" = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./users/alex.nix ];
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+        # extraSpecialArgs = { inherit inputs; };
+      };
+    }  # let ... in ...
+  );
 }
 
