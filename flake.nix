e3746fa578070ab1f6cbcb456f80fd5e11132f66
Alexander Leschanz <80907917+Creative-Difficulty@users.noreply.github.com>
2025-05-08 00:22:56 +0200

    chore: Fix typo

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Alex' (beginnner) NixOS configuration flake";
+  description = "Alex' (beginner) NixOS configuration flake";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
