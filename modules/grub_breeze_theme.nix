f9a3dc53f83c36433edf292e4ff84c773da48282
Alexander Leschanz <80907917+Creative-Difficulty@users.noreply.github.com>
2025-06-19 18:00:58 +0200

    fix: Grub breeze theme module

---
+++ modules/grub_breeze_theme.nix
@@ -0,0 +1,15 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
+  options = {
+    grub_theme.enable = lib.mkEnableOption "Whether to enable the breeze GRUB theme";
+  };
+
+  config = lib.mkIf config.grub_theme.enable {
+    boot.loader.grub.theme = "${pkgs.libsForQt5.breeze-grub}/grub/themes/breeze";
+  };
+}
